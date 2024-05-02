adfdad2541feb614972d118919a668d63f103f67
Piyapat <piyapat.pai@outlook.com>
2024-05-02 16:57:26 +0700

    login page prototype.

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useState } from 'react';
+import Link from 'next/link';
 
 const LoginPage = () => {
   return (
-    <div>
-      <div>Login</div>
-      
-    </div>
-  )
-}
+    <div className= 'bg-red-300'>
+      <form className='flex flex-col'>
+        <label className='text-center'>
+          Username:
+          <input type="text" username="Username" />
+        </label>
+        <label className='text-center'>
+          Password:
+          <input type="text" password="Password" />
+        </label>
+        <input type="submit" value="Submit" className='bg-blue-300'/>
+      </form>
+    </div>  
+    )}
 
 export default LoginPage
