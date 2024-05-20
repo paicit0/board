75d89ebb81c721b0c5a89945835849616d87af07
Piyapat <piyapat.pai@outlook.com>
2024-05-20 22:00:28 +0700

    added reply count.

--- src/app/api/createThread/route.js
+++ src/app/api/createThread/route.js
@@ -16,6 +16,7 @@ export async function POST(req) {
       title,
       threadContent,
       createdAt: new Date(),
+      replyCount: 0,
       threadUpvotes: 0,
       threadDownvotes: 0,
     });
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -27,11 +27,13 @@ export default function Home() {
   return (
     <main>
       <div className="flex-grow text-center p-10">
+        
         <Link className='bg-green-500 w-fit text-sm text-white py-3 px-5 rounded-md mt-2' href="/createThread">Submit Thread</Link>
         <div className="flex justify-center my-10">
           <div className="w-full max-w-7xl grid grid-cols-1 md:grid-cols-2 lg:grid-cols-8 gap-4">
             {threads.map((thread) => (
               <div key={thread._id} className="p-5 border-b flex flex-col">
+                <p>Reply: {thread.replyCount}</p>
                 <h2 className="text-2xl font-bold break-words line-clamp-4">{thread.title}</h2>
                 <p className="text-gray-600 break-words line-clamp-6">{thread.threadContent}</p>
                 <p className="text-sm text-gray-400 mt-auto">Date: {new Date(thread.createdAt).toLocaleString()}</p>
