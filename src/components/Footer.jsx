0446a3ef38513e2bb330a8ebe8160399d5422ae2
Piyapat <piyapat.pai@outlook.com>
2024-07-07 17:09:46 +0700

    added github on footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,6 @@ import Link from "next/link";
 const Footer = () => {
   return (
     <footer className="text-sm bg-blue-500 text-white text-center p-4 fixed inset-x-0 bottom-0">
-
       GITHUB: https://github.com/paicit0/board © 2024 BOARD. All rights reserved.
     </footer>
   );
