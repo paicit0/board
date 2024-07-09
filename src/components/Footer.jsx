cbeff4d9f3a84ca5bb5cdad8e4aaaf4b5c96182b
Piyapat <piyapat.pai@outlook.com>
2024-07-09 22:41:41 +0700

    footer github fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,9 +4,11 @@ import Link from "next/link";
 const Footer = () => {
   return (
     <footer className="text-sm bg-blue-500 text-white text-center p-4 fixed inset-x-0 bottom-0">
-      <div>GITHUB: https://github.com/paicit0/board © 2024 BOARD. All rights reserved.</div>
+      <div className="">
+      GITHUB: <Link className="font-bold" href="https://github.com/paicit0/board" target="_blank">https://github.com/paicit0/board</Link> © 2024 BOARD. All rights reserved.
+      </div>
       <div className="text-xl md:font-bold flex-1 md:text-center">
-        <Link href="/patch">Patch Notes</Link>
+        <Link href="/patch">Features</Link>
       </div>
     </footer>
   );
