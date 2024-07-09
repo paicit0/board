2fc8e4cd259af92b96fbc88e748d1888ecfe41de
Piyapat <piyapat.pai@outlook.com>
2024-07-09 22:37:27 +0700

    patchnote (fetch from github/readme)

---
+++ src/app/patch/page.jsx
@@ -0,0 +1,26 @@
+'use client'
+
+import { useEffect, useState } from 'react';
+import { marked } from 'marked';
+
+export default function Home() {
+  const [readme, setReadme] = useState('');
+
+  useEffect(() => {
+    async function fetchReadme() {
+      const response = await fetch('/api/readmeFetch');
+      const data = await response.json();
+      const html = marked(data.content);
+      setReadme(html);
+    }
+
+    fetchReadme();
+  }, []);
+
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-2xl font-bold mb-4 text-center">Patch Notes</h1>
+      <div className="prose" dangerouslySetInnerHTML={{ __html: readme }} />
+    </div>
+  );
+}
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,10 @@ import Link from "next/link";
 const Footer = () => {
   return (
     <footer className="text-sm bg-blue-500 text-white text-center p-4 fixed inset-x-0 bottom-0">
-      GITHUB: https://github.com/paicit0/board © 2024 BOARD. All rights reserved.
+      <div>GITHUB: https://github.com/paicit0/board © 2024 BOARD. All rights reserved.</div>
+      <div className="text-xl md:font-bold flex-1 md:text-center">
+        <Link href="/patch">Patch Notes</Link>
+      </div>
     </footer>
   );
 };
