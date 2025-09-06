09622cc96e338b5641ff27985572a42b4b09ff4d
Piyapat <piyapat.pai@outlook.com>
2025-09-07 02:10:14 +0700

    fix: register api using local url for some reason

--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -44,7 +44,7 @@ function RegisterPage() {
     }
 
     try {
-      const res = await fetch("http://localhost:3000/api/register", {
+      const res = await fetch("/api/register", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
